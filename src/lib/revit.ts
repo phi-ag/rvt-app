23a1449253040775f21ba3c9a0b0cd752b6541be
Peter Hirn <peter.hirn@phi.ag>
2024-11-11 14:57:28 +0100

    refactor: parse app name and content

--- src/lib/revit.ts
+++ src/lib/revit.ts
@@ -1,5 +1,7 @@
 const decoder = new TextDecoder("utf-16le");
 
+export type FileVersion = 10 | 13 | 14;
+
 const findVersionMarker = (data: Uint8Array): number | undefined => {
   const versionMarker = [0x04, 0x00, 0x00, 0x00];
 
@@ -20,7 +22,6 @@ type ParseVersionResult = [version: string, build: string, end: number];
 const parseVersion10 = (data: Uint8Array, view: DataView): ParseVersionResult => {
   const versionLengthStart = 14;
   const versionLength = view.getInt32(versionLengthStart, true);
-  console.log(versionLength);
 
   const versionStart = versionLengthStart + 4;
   const versionEnd = versionStart + versionLength * 2;
@@ -48,7 +49,7 @@ const parseVersion13 = (data: Uint8Array, view: DataView): ParseVersionResult =>
 };
 
 const parseVersion = (
-  fileVersion: number,
+  fileVersion: FileVersion,
   data: Uint8Array,
   view: DataView
 ): ParseVersionResult => {
@@ -66,15 +67,15 @@ const parsePath = (
   view: DataView,
   position: number
 ): ParsePathResult => {
-  const lenght = view.getInt32(position, true);
+  const length = view.getInt32(position, true);
   const start = position + 4;
-  const end = start + lenght * 2;
+  const end = start + length * 2;
   const path = decoder.decode(data.subarray(start, end));
 
   return [path, end];
 };
 
-type ParseGuidsResult = [guid1: string, guid2: string, end: number];
+type ParseGuidsResult = [identityId: string, documentId: string, end: number];
 
 const parseGuids = (
   data: Uint8Array,
@@ -117,23 +118,74 @@ const parseGuids = (
   return [guid1, guid2, guid4End] as const;
 };
 
+type ParseAppNameResult = [name1: string | undefined, end: number];
+
+const parseAppName = (
+  fileVersion: FileVersion,
+  data: Uint8Array,
+  view: DataView,
+  position: number
+): ParseAppNameResult => {
+  if (fileVersion === 10) return [undefined, position];
+
+  //const _unknownFlag = view.getInt8(position);
+  const appNameLength = view.getInt32(position + 1, true);
+  const appNameStart = position + 1 + 4;
+  const appNameEnd = appNameStart + appNameLength * 2;
+  const appName = decoder.decode(data.subarray(appNameStart, appNameEnd));
+
+  if (fileVersion === 13) return [appName, appNameEnd];
+
+  const appName2Length = view.getInt32(appNameEnd, true);
+  const appName2Start = appNameEnd + 4;
+  const appName2End = appName2Start + appName2Length * 2;
+  //const appName2 = decoder.decode(data.subarray(appName2Start, appName2End));
+
+  return [appName, appName2End];
+};
+
+type ParseContentStart = [start: number, end: number];
+
+const parseContentStart = (
+  fileVersion: FileVersion,
+  data: Uint8Array,
+  view: DataView,
+  position: number
+): ParseContentStart => {
+  if (fileVersion === 10 || fileVersion === 13)
+    return [position + 2, data.byteLength - 2] as const;
+
+  const unknownPaddingFlag = view.getInt8(position);
+  const padding = unknownPaddingFlag === 4 ? 5 : 2;
+  return [position + padding, data.byteLength - padding] as const;
+};
+
+const parseContent = (
+  fileVersion: FileVersion,
+  data: Uint8Array,
+  view: DataView,
+  position: number
+): string => {
+  const [start, end] = parseContentStart(fileVersion, data, view, position);
+  return decoder.decode(data.subarray(start, end));
+};
+
 export interface FileInfo {
-  fileVersion: number;
+  fileVersion: FileVersion;
   version: string;
   build: string;
   path: string;
-  guid1: string;
-  guid2: string;
+  identityId: string;
+  documentId: string;
   appName?: string;
-  appName2?: string;
   content: string;
 }
 
 /**
  * Parse BasicFileInfo
  *
- * This was implemented while staring at a hex editor and guessing what each byte could mean.
- * Therefore very likely incomplete and buggy.
+ * This was implemented while staring at a hex editor and guessing what each byte could mean,
+ * therefore very likely incomplete and buggy. 🤷
  *
  * File versions:
  * - 10 -> Revit 2017/2018
@@ -142,61 +194,24 @@ export interface FileInfo {
  */
 export const parseBasicFileInfo = (data: Uint8Array): FileInfo => {
   const view = new DataView(data.buffer, data.byteOffset);
-  const fileVersion = view.getInt16(0, true);
+  const fileVersion = view.getInt32(0, true);
   if (fileVersion !== 10 && fileVersion !== 13 && fileVersion !== 14)
     throw Error(`Unknown file version ${fileVersion}`);
 
   const [version, build, versionEnd] = parseVersion(fileVersion, data, view);
   const [path, pathEnd] = parsePath(data, view, versionEnd);
-  const [guid1, guid2, guidsEnd] = parseGuids(data, view, pathEnd);
-  //const _unknownFlag = view.getInt8(guidsEnd);
-
-  let appName!: string;
-  let appNameEnd!: number;
-
-  if (fileVersion !== 10) {
-    const appNameLength = view.getInt32(guidsEnd + 1, true);
-    const appNameStart = guidsEnd + 1 + 4;
-    appNameEnd = appNameStart + appNameLength * 2;
-    appName = decoder.decode(data.subarray(appNameStart, appNameEnd));
-  }
-
-  let appName2!: string;
-  let contentStart!: number;
-  let contentTrim!: number;
-
-  if (fileVersion === 10) {
-    contentTrim = 2;
-    contentStart = guidsEnd + 2;
-  } else if (fileVersion === 13) {
-    contentTrim = 2;
-    contentStart = appNameEnd + 2;
-  } else if (fileVersion === 14) {
-    const appName2Length = view.getInt32(appNameEnd, true);
-    const appName2Start = appNameEnd + 4;
-    const appName2End = appName2Start + appName2Length * 2;
-    appName2 = decoder.decode(data.subarray(appName2Start, appName2End));
-
-    const unknownPaddingFlag = view.getInt8(appName2End);
-    const padding = unknownPaddingFlag === 4 ? 5 : 2;
-    contentTrim = unknownPaddingFlag === 4 ? 5 : 2;
-
-    contentStart = appName2End + padding;
-  }
-
-  const content = decoder.decode(
-    data.subarray(contentStart, data.byteLength - contentTrim)
-  );
+  const [identityId, documentId, guidsEnd] = parseGuids(data, view, pathEnd);
+  const [appName, appNameEnd] = parseAppName(fileVersion, data, view, guidsEnd);
+  const content = parseContent(fileVersion, data, view, appNameEnd);
 
   return {
     fileVersion,
     version,
     build,
     path,
-    guid1,
-    guid2,
+    identityId,
+    documentId,
     appName,
-    appName2,
     content
   };
 };
