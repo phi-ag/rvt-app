c9ce5a55d1fe8655e18ff229f27a38ce45fcb27a
Peter Hirn <peter.hirn@phi.ag>
2024-11-17 20:25:07 +0100

    refactor: move cfb functions

--- src/lib/cfb.ts
+++ src/lib/cfb.ts
@@ -22,39 +22,6 @@ export enum EntryType {
   RootStorage = 5
 }
 
-const assertEqual = (
-  chunk: Uint8Array,
-  position: number,
-  expected: ArrayLike<number>,
-  message: string
-) => {
-  const data = chunk.subarray(position, position + expected.length);
-  if (!array.isEqual(data, expected))
-    throw Error(`Unexpected compound file ${message} [${data}]`);
-};
-
-const assertZero = (
-  chunk: Uint8Array,
-  position: number,
-  length: number,
-  message: string
-) => {
-  const data = chunk.subarray(position, position + length);
-  if (!array.isZero(data))
-    throw Error(`Unexpected compound file non-zero ${message} [${data}]`);
-};
-
-// see https://github.com/SheetJS/js-cfb/blob/master/cfb.js#L669
-export const readDate = (view: DataView, offset: number) => {
-  const leftShift32Factor = 4294967296; // Math.pow(2, 32)
-  const filetimeOffset = 116444736e5;
-
-  const low = view.getUint32(offset, true);
-  const high = view.getUint32(offset + 4, true);
-
-  return new Date((high * leftShift32Factor + low) / 1e4 - filetimeOffset);
-};
-
 export type Boundary = [start: number, end: number];
 
 export type BoundaryWithOffset = [start: number, end: number, offset: number];
@@ -89,10 +56,50 @@ export class Boundaries {
 }
 
 const signature = [0xd0, 0xcf, 0x11, 0xe0, 0xa1, 0xb1, 0x1a, 0xe1];
+const endOfChain = 0xfffffffe;
+const maxFatSectorsHeader = 109;
 
-export const freeSector = 0xffffffff;
-export const endOfChain = 0xfffffffe;
-export const maxFatSectorsHeader = 109;
+export const sliceUint8 = async (
+  blob: Blob,
+  start: number,
+  end: number
+): Promise<Uint8Array> => new Uint8Array(await blob.slice(start, end).arrayBuffer());
+
+const sliceView = async (blob: Blob, start: number, end: number): Promise<DataView> =>
+  new DataView(await blob.slice(start, end).arrayBuffer());
+
+const assertEqual = (
+  chunk: Uint8Array,
+  position: number,
+  expected: ArrayLike<number>,
+  message: string
+) => {
+  const data = chunk.subarray(position, position + expected.length);
+  if (!array.isEqual(data, expected))
+    throw Error(`Unexpected compound file ${message} [${data}]`);
+};
+
+const assertZero = (
+  chunk: Uint8Array,
+  position: number,
+  length: number,
+  message: string
+) => {
+  const data = chunk.subarray(position, position + length);
+  if (!array.isZero(data))
+    throw Error(`Unexpected compound file non-zero ${message} [${data}]`);
+};
+
+// see https://github.com/SheetJS/js-cfb/blob/master/cfb.js#L669
+export const readDate = (view: DataView, offset: number) => {
+  const leftShift32Factor = 4294967296; // Math.pow(2, 32)
+  const filetimeOffset = 116444736e5;
+
+  const low = view.getUint32(offset, true);
+  const high = view.getUint32(offset + 4, true);
+
+  return new Date((high * leftShift32Factor + low) / 1e4 - filetimeOffset);
+};
 
 export type Version = 3 | 4;
 
@@ -283,4 +290,278 @@ export const parseDirectory = (header: Header, sectors: Uint8Array): Directory =
   }
 
   return entries;
+};
+
+export const findEntry = (directory: Directory, name: string) =>
+  directory.find((entry) => entry.name === name);
+
+const fatSectorChain = (
+  name: string,
+  fat: Uint32Array,
+  start: number,
+  length: number
+): Uint32Array => {
+  const sectors = new Uint32Array(length);
+
+  for (let i = 0, current = start; ; i++) {
+    if (current === endOfChain) {
+      if (i !== length)
+        throw Error(`${name} unexpected end of fat chain (${i} !== ${length})`);
+      break;
+    }
+
+    if (i >= length) throw Error(`${name} fat chain too long (${i} >= ${length})`);
+    sectors[i] = current;
+
+    if (current >= fat.length)
+      throw Error(`${name} fat chain out-of-bounds (${current} >= ${fat.length})`);
+    current = fat[current];
+  }
+
+  return sectors;
+};
+
+const directorySectorChain = (header: Header, fat: Uint32Array): Uint32Array =>
+  fatSectorChain("Directory", fat, header.directoryStart, header.directorySectorCount);
+
+const miniFatSectorChain = (header: Header, fat: Uint32Array): Uint32Array =>
+  fatSectorChain("MiniFat", fat, header.miniFatStart, header.miniFatSectorCount);
+
+export const miniStreamSectorChain = (
+  header: Header,
+  fat: Uint32Array,
+  root: Entry
+): Uint32Array => {
+  const sectorCount = (root.size + header.sectorSize - 1) >> header.sectorSizeBits;
+  return fatSectorChain("MiniStream", fat, root.start, sectorCount);
+};
+
+const sectorBounds = (
+  header: Header,
+  sectors: Uint32Array,
+  byteLength?: number
+): Boundaries => {
+  const bounds = new Boundaries();
+  const sectorSize = header.sectorSize;
+
+  for (let i = 0; i < sectors.length; i++) {
+    const start = (sectors[i] + 1) * sectorSize;
+    const size = byteLength
+      ? Math.min(byteLength - i * sectorSize, sectorSize)
+      : sectorSize;
+
+    const end = start + size;
+    bounds.add([start, end]);
+  }
+
+  return bounds;
+};
+
+const boundsData = async (blob: Blob, bounds: Boundaries): Promise<Uint8Array> => {
+  const data = new Uint8Array(bounds.size());
+
+  for (const [start, end, offset] of bounds.items) {
+    const chunk = await sliceUint8(blob, start, end);
+    data.set(chunk, offset);
+  }
+
+  return data;
+};
+
+const boundsEntries = async (blob: Blob, bounds: Boundaries): Promise<Uint32Array> => {
+  const entries = new Uint32Array(bounds.size() >> 2);
+
+  for (const [start, end, offset] of bounds.items) {
+    const view = await sliceView(blob, start, end);
+    const chunkEntries = view.byteLength >> 2;
+    const chunkOffset = offset >> 2;
+    for (let i = 0; i < chunkEntries; i++) {
+      entries[i + chunkOffset] = view.getUint32(i * 4, true);
+    }
+  }
+
+  return entries;
+};
+
+export const createDirectory = async (
+  blob: Blob,
+  header: Header,
+  fat: Uint32Array
+): Promise<Directory> => {
+  const sectors = directorySectorChain(header, fat);
+  const bounds = sectorBounds(header, sectors);
+  const data = await boundsData(blob, bounds);
+  return parseDirectory(header, data);
+};
+
+const fatBounds = (
+  header: Header,
+  fat: Uint32Array,
+  start: number,
+  size: number
+): Boundaries => {
+  const bounds = new Boundaries();
+
+  for (let i = 0, current = start, remaining = size; ; i++) {
+    if (current === endOfChain) {
+      if (remaining !== 0) throw Error("Fat bounds unexpected end of chain");
+      break;
+    }
+
+    const start = (current + 1) * header.sectorSize;
+    const size = remaining < header.sectorSize ? remaining : header.sectorSize;
+    remaining -= size;
+
+    bounds.add([start, start + size]);
+
+    if (current >= fat.length) throw Error(`Fat out-of-bounds (${current})`);
+    current = fat[current];
+  }
+
+  return bounds;
+};
+
+const miniStreamOffset = (
+  header: Header,
+  miniStreamSectors: Uint32Array,
+  start: number
+) => {
+  const subBits = header.sectorSizeBits - header.miniSectorSizeBits;
+  const sector = start >> subBits;
+  if (sector >= miniStreamSectors.length)
+    throw Error(`MiniStream sector out-of-bounds (${sector})`);
+
+  // Some kind of magic 👀
+  // see https://github.com/p7zip-project/p7zip/blob/master/CPP/7zip/Archive/ComHandler.cpp#L152
+  const offset =
+    ((miniStreamSectors[sector] + 1) << subBits) + (start & ((1 << subBits) - 1));
+
+  return offset * header.miniSectorSize;
+};
+
+const miniStreamBounds = (
+  header: Header,
+  miniFat: Uint32Array,
+  miniStreamSectors: Uint32Array,
+  start: number,
+  size: number
+): Boundaries => {
+  const bounds = new Boundaries();
+
+  for (let i = 0, current = start, remaining = size; ; i++) {
+    if (current === endOfChain) {
+      if (remaining !== 0) throw Error("MiniStream bounds unexpected end of chain");
+      break;
+    }
+
+    const start = miniStreamOffset(header, miniStreamSectors, current);
+
+    const size = remaining < header.miniSectorSize ? remaining : header.miniSectorSize;
+    remaining -= size;
+
+    bounds.add([start, start + size]);
+
+    if (current >= miniFat.length) throw Error(`MiniStream out-of-bounds (${current})`);
+    current = miniFat[current];
+  }
+
+  return bounds;
+};
+
+const miniStreamData = async (
+  blob: Blob,
+  header: Header,
+  miniFat: Uint32Array,
+  miniStreamSectors: Uint32Array,
+  start: number,
+  size: number
+): Promise<Uint8Array> => {
+  const bounds = miniStreamBounds(header, miniFat, miniStreamSectors, start, size);
+  return await boundsData(blob, bounds);
+};
+
+/**
+ * NOTE: Never tested with more than one DIFAT sector
+ */
+export const addDiFatEntries = async (
+  blob: Blob,
+  header: Header,
+  fatSectors: Uint32Array
+) => {
+  for (let i = 0, current = header.diFatStart; ; i++) {
+    if (current === endOfChain) {
+      if (i !== header.diFatCount)
+        throw Error(`Unexpected end of diFat chain (${i} !== ${header.diFatCount})`);
+      break;
+    }
+
+    const diFatStart = (current + 1) * header.sectorSize;
+    const diFatEnd = diFatStart + header.sectorSize;
+    const diFat = await sliceView(blob, diFatStart, diFatEnd);
+
+    const max = header.maxSectorEntries - 1;
+    const offset = i * max + maxFatSectorsHeader;
+
+    const remaining = Math.min(header.fatSectorCount - offset, max);
+    if (remaining < 0)
+      throw Error(`Unexpected remaining sectors in diFat chain (${remaining})`);
+
+    for (let j = 0; j < remaining; j++) {
+      fatSectors[j + offset] = diFat.getUint32(j * 4, true);
+    }
+
+    current = diFat.getUint32(max * 4, true);
+  }
+};
+
+export const createFat = async (
+  blob: Blob,
+  header: Header,
+  fatSectors: Uint32Array
+): Promise<Uint32Array> => {
+  const fatBounds = sectorBounds(header, fatSectors);
+  return await boundsEntries(blob, fatBounds);
+};
+
+/**
+ * NOTE: Never tested with more than one MiniFat sector
+ */
+export const createMiniFat = async (
+  blob: Blob,
+  header: Header,
+  fat: Uint32Array,
+  root: Entry
+): Promise<Uint32Array> => {
+  const miniFatSectors = miniFatSectorChain(header, fat);
+  const miniFatCount =
+    (root.size + (1 << header.miniSectorSizeBits) - 1) >> header.miniSectorSizeBits;
+
+  const miniFatBounds = sectorBounds(header, miniFatSectors, miniFatCount * 4);
+  return await boundsEntries(blob, miniFatBounds);
+};
+
+/**
+ * NOTE: Argument count is getting out of hand
+ */
+export const entryData = async (
+  blob: Blob,
+  header: Header,
+  fat: Uint32Array,
+  miniFat: Uint32Array,
+  miniStreamSectors: Uint32Array,
+  entry: Entry
+): Promise<Uint8Array> => {
+  if (entry.size < header.miniFatCutoff) {
+    return await miniStreamData(
+      blob,
+      header,
+      miniFat,
+      miniStreamSectors,
+      entry.start,
+      entry.size
+    );
+  }
+
+  const bounds = fatBounds(header, fat, entry.start, entry.size);
+  return await boundsData(blob, bounds);
 };
